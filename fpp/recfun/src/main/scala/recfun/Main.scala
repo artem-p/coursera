0d904a02b009292b560971c9d40ccca7b570c183
artem-p <artempugachev1@gmail.com>
2016-06-27 16:13:55 +0300

    to main

--- fpp/recfun/src/main/scala/recfun/Main.scala
+++ fpp/recfun/src/main/scala/recfun/Main.scala
@@ -11,19 +11,43 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int =
-      if (c==0 || c==r) 1
-      else pascal(c - 1, r - 1) + pascal( c, r -1)
-  
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int =
+    if (c==0 || c==r) 1
+    else pascal(c - 1, r - 1) + pascal( c, r -1)
+
   /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    def only_parentheses(chars: List[Char]): List[Char] =
+      chars.filter(is_parentheses)
+
+    def is_parentheses(c: Char): Boolean =
+      if (c == '(' || c == ')') true else false
+
+    def count_pars(pars: List[Char]): Int =
+    // returns +1 when first par is "(" and -1 when ")"
+      if (pars.nonEmpty)
+        if (pars.head == '(') 1
+        else -1
+      else 0
+
+    def is_balanced(pars: List[Char], numOpen: Int): Boolean =
+      if (pars.isEmpty)
+        if (numOpen == 0) true else false
+      else {
+        val nowOpen = numOpen + count_pars(pars)
+        is_balanced(pars.tail, nowOpen)
+      }
+
+
+    is_balanced(only_parentheses(chars), 0)
   }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
